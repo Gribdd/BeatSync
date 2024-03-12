7af0a2f939d890e56f1624e70b4d319ca2ed84db
SOPHIA KYLA MAGADAN <sophiakyla.magadan.21@usjr.edu.ph>
2024-03-12 20:19:37 +0800

    Updated ArtistManagement Page UI

--- BeatSync/Pages/ArtistManagement.xaml.cs
+++ BeatSync/Pages/ArtistManagement.xaml.cs
@@ -6,4 +6,18 @@ public partial class ArtistManagement : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnBtnAddClicked(object sender, EventArgs e)
+    {
+        
+    }
+
+    private void OnBtnDeleteClicked(object sender, EventArgs e)
+    {
+
+    }
+    private void OnBtnUpdateClicked(object sender, EventArgs e)
+    {
+
+    }
 }
