007a70a7622855861ee27a2fb336728df11c08e0
SOPHIA KYLA MAGADAN <sophiakyla.magadan.21@usjr.edu.ph>
2024-03-26 20:59:42 +0800

    Added working Search Functionality without UI.

--- BeatSync/ViewModel/Users/SearchPageViewModel.cs
+++ BeatSync/ViewModel/Users/SearchPageViewModel.cs
@@ -1,26 +1,104 @@
+using BeatSync.Models;
 using BeatSync.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+using System.Threading.Tasks;
 
 namespace BeatSync.ViewModel.Users;
 
 public partial class SearchPageViewModel : ObservableObject
 {
-	private AdminService _adminService; 
-	public SearchPageViewModel(AdminService adminService)
-	{
-		_adminService = adminService;
-	}
 
-	[RelayCommand]
-	async Task Logout()
-	{
+    [ObservableProperty]
+    private string? _searchQuery;
+
+    [ObservableProperty]
+    private MyCollection _myCollection = new();
+
+
+    [ObservableProperty]
+    private ObservableCollection<object> _myList = new();
+
+    private AdminService _adminService;
+    private ArtistService _artistService;
+    private AlbumService _albumService;
+    private PublisherService _publisherService;
+    private SongService _songService;
+    private UserService _userService;
+
+    public SearchPageViewModel(AdminService adminService, ArtistService artistService, AlbumService albumService, PublisherService publisherService, SongService songService, UserService userService)
+    {
+        _adminService = adminService;
+        _artistService = artistService;
+        _albumService = albumService;
+        _publisherService = publisherService;
+        _songService = songService;
+        _userService = userService;
+    }
+
+    [RelayCommand]
+    async Task Logout()
+    {
         await _adminService.Logout();
     }
+
+    [RelayCommand]
+    public async Task SearchCommand()
+    {
+        _myCollection.Filter(SearchQuery);
+        MyList.Clear(); 
+
+        bool found = false; 
+        foreach (var item in _myCollection.FilteredCollection)
+        {
+            switch (item)
+            {
+                case BeatSync.Models.Song song when song.Name.Contains(SearchQuery):
+                    MyList.Add(String.Concat(SearchQuery, " - Song"));
+                    found = true;
+                    break;
+                case BeatSync.Models.Album album when album.Name.Contains(SearchQuery):
+                    MyList.Add(String.Concat(SearchQuery, " - Album"));
+                    found = true;
+                    break;
+                case BeatSync.Models.Artist artist when artist.FullName.Contains(SearchQuery):
+                    MyList.Add(String.Concat(SearchQuery, " - Artist"));
+                    found = true;
+                    break;
+                case BeatSync.Models.Artist artist when artist.Username.Contains(SearchQuery):
+                    MyList.Add(String.Concat(SearchQuery, " - Artist"));
+                    found = true;
+                    break;
+                case BeatSync.Models.Publisher publisher when publisher.FullName.Contains(SearchQuery):
+                    MyList.Add(String.Concat(SearchQuery, " - Publisher"));
+                    found = true;
+                    break;
+                case BeatSync.Models.Publisher publisher when publisher.Username.Contains(SearchQuery):
+                    MyList.Add(String.Concat(SearchQuery, " - Publisher"));
+                    found = true;
+                    break;
+                case BeatSync.Models.User user when user.FullName.Contains(SearchQuery):
+                    MyList.Add(String.Concat(SearchQuery, " - User"));
+                    found = true;
+                    break;
+                case BeatSync.Models.User user when user.Username.Contains(SearchQuery):
+                    MyList.Add(String.Concat(SearchQuery, " - User"));
+                    found = true;
+                    break;
+            }
+        }
+
+        // If no items were found, add default message
+        if (!found)
+        {
+            string foundNothing = "No search result found.";
+            MyList.Add(foundNothing);
+        }
+    }
+
+
+
 }
