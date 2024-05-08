3d75f08f923b5c5d2071f72c7433c3ec06dcd53a
SOPHIA KYLA MAGADAN <sophiakyla.magadan.21@usjr.edu.ph>
2024-05-08 16:18:55 +0800

    Fixed Binding on Flyout

--- BeatSync/Views/ArtistLandingPage.xaml.cs
+++ BeatSync/Views/ArtistLandingPage.xaml.cs
@@ -10,6 +10,8 @@ public partial class ArtistLandingPage : Shell
         Routing.RegisterRoute($"songs/{nameof(AddSong)}", typeof(AddSong));
         Routing.RegisterRoute($"library/{nameof(AddAlbumPublisher)}", typeof(AddAlbumPublisher)); //add/revise for artist side
         Routing.RegisterRoute($"library/{nameof(AddAlbumSongs)}", typeof(AddAlbumSongs));
+        Routing.RegisterRoute($"library/{nameof(PubRecentlyPlayed)}",typeof(PubRecentlyPlayed)); //add/revise for artist side
+        Routing.RegisterRoute($"library/{nameof(CustomerFavoriteSongs)}", typeof(CustomerFavoriteSongs)); //add/revise for artist side
         Routing.RegisterRoute(nameof(ArtistViewProfile), typeof(ArtistViewProfile));
 
         BindingContext = _vm = vm;
@@ -19,6 +21,7 @@ public partial class ArtistLandingPage : Shell
     protected async override void OnAppearing()
     {
         base.OnAppearing();
-        await _vm.GetActiveArtist();
+        //await _vm.GetActiveArtist();
+        var result = Task.Run(async () => await _vm.GetActiveArtist());
     }
 }
